a1af72a817da2f6d32a1c6d7880bb3455ac277ba
tanonibrenda <brendayohenatanoni@gmail.com>
2024-11-11 18:07:34 -0300

    frontCarpetas

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+// Definir las rutas relacionadas con las tareas
+router.get('/', taskController.getAllTasks);
+router.post('/', taskController.createTask);
+router.get('/:id', taskController.getTaskById);
+router.put('/:id', taskController.updateTask);
+router.delete('/:id', taskController.deleteTask);
+
+module.exports = router;
